0da745610785e4b2f04544402b84528170ff23d0
somestuds <somestudsdev@gmail.com>
2025-12-23 02:30:27 -0500

    IDE Changes, dw about it

---
+++ install-xeno-updater.cmd
@@ -0,0 +1,21 @@
+@echo off
+mkdir "bin"
+mkdir "bin\xeno-updater"
+
+set parent=bin\xeno-updater
+del /Q "%parent%\*"
+for /d %%D in ("%parent%\*") do rmdir /S /Q "%%D"
+
+xcopy "_install\*" "%parent%" /E /I /Y
+rmdir /S /Q "_install"
+pushd %parent%
+npm install
+popd
+
+del /Q "update-xeno.cmd"
+echo @echo off >> "update-xeno.cmd"
+echo node "%%~dp0\bin\xeno-updater\index.js" %%* >> "update-xeno.cmd"
+
+@echo on
+echo Xeno Updater installed properly and correctly.
+pause
